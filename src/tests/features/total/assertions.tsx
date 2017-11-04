c6968fb87780cbfe61b3c31eeac7940f04ef841c
Victor Magalhães <victor@cubostecnologia.com.br>
2017-11-04 13:52:59 -0300

    Test for category total

--- src/tests/features/total/assertions.tsx
+++ src/tests/features/total/assertions.tsx
@@ -2,34 +2,57 @@ import DataTableRow from "../../../components/row";
 import "jest";
 import "../../setup";
 import * as React from "react";
-import { mount } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import { expect } from "chai";
 import DataTable, { IDataTableProps } from "../../../";
-import DataTableBody from "../../../components/body";
+import DataTableBody, { DataTableCategorySection } from "../../../components/body";
+import { IDataTableRowProps } from "../../../components/row";
+
+function testForHiddenTotal(wrapper: ReactWrapper, shouldHide: boolean | undefined, kind: "body" | "category") {
+	const propName = kind === "body" ? "hideBodyTotal" : kind === "category" ? "hideCategoryTotal" : undefined;
+
+	if (!propName) throw new Error("Invalid call for testForHiddenTotal: kind should be body or category, but is " + kind);
+
+	describe("since has totalAccessor and one or more rows", () => {
+		const totalRowWrapper = wrapper.findWhere((el) => {
+			const isCategory = (el.props() as IDataTableRowProps).isCategory;
+
+			return el.is(DataTableRow) && el.is({ isAggregated: true }) && (kind === "body" ? !isCategory : !!isCategory);
+		});
+
+		if (shouldHide) {
+			describe(`since ${propName} is true`, () => {
+				it("shouldn't render a DataTableRow with isAggregated={true}", () => {
+					expect(totalRowWrapper.exists()).to.be.false;
+				});
+			});
+		} else {
+			describe(`since ${propName} is false`, () => {
+				it("should render a DataTableRow with isAggregated={true}", () => {
+					expect(totalRowWrapper).to.have.length(1);
+				});
+			});
+		}
+	});
+}
 
 export function testDataTableTotalFeatureWithProps<RowData extends object>(props: IDataTableProps<RowData>) {
 	const wrapper = mount(<DataTable {...props}/>);
 
 	if (props.totalAccessor) {
-		const firstBodyChild = wrapper.find(DataTableBody).find("tbody").childAt(0);
+		const tableBody = wrapper.find(DataTableBody).find("tbody");
 
-		if (firstBodyChild.exists()) {
-			describe("since has totalAccessor and one or more rows", () => {
-				if (props.hideBodyTotal) {
-					describe("since hideBodyTotal is false", () => {
-						it("shouldn't render a DataTableRow with isAggregated={true} as first child", () => {
-							expect(!firstBodyChild.is(DataTableRow) || !firstBodyChild.is({ isAggregated: true })).to.be.true;
-						});
-					});
-				} else {
-					describe("since hideBodyTotal is true", () => {
-						it("should render a DataTableRow with isAggregated={true} as first child", () => {
-							expect(firstBodyChild.is(DataTableRow), "isn't DataTableRow").to.be.true;
-							expect(firstBodyChild.is({ isAggregated: true }), "hasn't isAggregated").to.be.true;
-						});
-					});
-				}
-			});
+		if (tableBody.children().length > 0) {
+			testForHiddenTotal(tableBody, props.hideBodyTotal, "body");
+
+			const categorySections = wrapper.find(DataTableCategorySection).map((v) => v);
+			for (const categorySection of categorySections) {
+				describe(`For category '${categorySection.props().category}'`, () => {
+					if (!categorySection.isEmpty()) {
+						testForHiddenTotal(categorySection, props.hideCategoryTotal, "category");
+					}
+				});
+			}
 		}
 	}
 }
